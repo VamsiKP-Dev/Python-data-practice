27f16a4664ced5ccf0cd4cedc905215a22c8bf14
Vamsi Krishna <vamsikrishnap@hotmail.com>
2025-08-09 18:46:02 +0530

    Add files via upload

---
+++ Python-data-practice.py
@@ -0,0 +1,55 @@
+
+"good morning"
+
+#data  -identified symbol
+greeting="good morning"
+
+#data -acessing variables
+momineedcoffee="Bru coffee"
+momineedcoffee="Tea"
+
+whatisbreakfast ="Idly"
+
+quantity=5
+#data -separated data tuple
+plate="5 idly","Sambar","Chetney"
+
+#logic
+youhadidly=quantity+3
+
+lunchbox="rice sambar snacks fruits"
+
+#data - seperated data tuple - fixed
+
+    #         0      1       2         3
+lunchbox = "rice","sambar","snacks","fruits"
+lunchbox[2]
+
+
+#data - job
+
+bustop="ganesha temple new thippasandra"
+timing="8:30 am"
+busno=314
+mystop="Bagmane tech park"
+company="Volvo"
+
+#data-office List
+office=["Laptop bag","your id card","mobilephone","lunch box"]
+office.append("friend")
+
+
+# Office team -dict
+vamsi="sde1"
+venkatesh="sde2"
+suresh="sde3"
+
+
+dicts={"vamsi":"sde1","venkatesh":"sde2","suresh":"sde3"}
+#        key              key               key -------> Keys is fixed cannot chaged.
+
+print(dicts["venkatesh"])
+
+dicts["venkatesh"]="sde6"
+print(dicts)
+
